ff43db06076eeb049da1398da275c5ed2d348a15
PieterGcode <pietergimbel@gmail.com>
2022-02-09 11:02:50 +0100

    improve explicit (voodoo) waits by checking incremental waits

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,11 @@ from selenium import webdriver
 import unittest
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 5
+
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self) -> None:
@@ -15,6 +19,17 @@ class NewVisitorTest(LiveServerTestCase):
     def check_for_row_in_list_table(self, row_text):
         rows = self.browser.find_elements(By.CSS_SELECTOR, "#id_list_table tr")
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                self.check_for_row_in_list_table(row_text)
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Joe wants to check out the homepage of the to-do app
@@ -35,19 +50,17 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting him to add another item. He
         # enters "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element(value="id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on his list
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # There is still a text box inviting her to add another item. He enters "Use peacock feathers to make a fly"
         # (Joe is very methodical)
